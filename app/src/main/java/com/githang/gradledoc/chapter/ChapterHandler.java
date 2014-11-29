9d1db76351518f3a48236ba514c339012f615708
msdx <645079761@qq.com>
2014-11-29 21:52:34 +0800

    Update UI

---
+++ app/src/main/java/com/githang/gradledoc/chapter/ChapterHandler.java
@@ -0,0 +1,37 @@
+package com.githang.gradledoc.chapter;
+
+import com.githang.gradledoc.datasource.AbstractResponse;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+/**
+ * User: Geek_Soledad(msdx.android@qq.com)
+ * Date: 2014-11-29
+ * Time: 21:36
+ * FIXME
+ */
+public abstract class ChapterHandler implements AbstractResponse {
+    @Override
+    public void onSuccess(String response) {
+        Document doc = Jsoup.parse(response);
+        Element chapter = doc.select("div.chapter").first();
+        String title = chapter.select("div.titlepage").select("h1").text();
+        chapter.removeClass("titlepage");
+
+        onResult(title, chapter.html());
+    }
+
+    public abstract void onResult(String title, String content);
+
+    @Override
+    public void onFailure(String response, Throwable e) {
+
+    }
+
+    @Override
+    public void onFinish() {
+
+    }
+}
