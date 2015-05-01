950b3a532088383659354f7e95b4d77ae4009843
msdx <645079761@qq.com>
2015-05-01 12:12:39 +0800

    修改系统状态栏及导航栏的颜色为actionbar的颜色

--- app/src/main/java/com/githang/gradledoc/common/BaseActivity.java
+++ app/src/main/java/com/githang/gradledoc/common/BaseActivity.java
@@ -1,6 +1,5 @@
 package com.githang.gradledoc.common;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
@@ -10,6 +9,7 @@ import com.readystatesoftware.systembartint.SystemBarTintManager;
  * @author Geek_Soledad (msdx.android@qq.com)
  */
 public class BaseActivity extends ActionBarActivity {
+    private static final int ACTION_BAR_BG = 0xffeeeeee;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +18,7 @@ public class BaseActivity extends ActionBarActivity {
         tintManager.setStatusBarTintEnabled(true);
         tintManager.setNavigationBarTintEnabled(true);
         // set a custom tint color for all system bars
-        tintManager.setTintColor(Color.GRAY);
-        tintManager.setNavigationBarTintColor(Color.GRAY);
+        tintManager.setTintColor(ACTION_BAR_BG);
+        tintManager.setNavigationBarTintColor(ACTION_BAR_BG);
     }
 }
