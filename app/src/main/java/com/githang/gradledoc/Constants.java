8893f95662d886184da2c3897da71de2afdd9846
msdx <645079761@qq.com>
2018-12-19 09:32:43 +0800

    Modify base url

--- app/src/main/java/com/githang/gradledoc/Constants.java
+++ app/src/main/java/com/githang/gradledoc/Constants.java
@@ -10,7 +10,7 @@ public interface Constants {
     String TITLE = "title";
     String URL = "url";
 
-    String BASE_URL = "http://gradledoc.qiniudn.com/1.12/userguide/";
+    String BASE_URL = "http://gradledoc.githang.com/1.12/userguide/";
     String USER_GUIDE = BASE_URL + "userguide.html";
     String URL_CONTRIBUTORS = "https://api.github.com/repos/msdx/gradledoc/contributors";
     String URL_PROCESS = "https://github.com/msdx/gradledoc/commits/1.12";
